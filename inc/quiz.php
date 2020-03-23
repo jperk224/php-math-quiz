b2023d416c8b9f890f2a4ef9b8a7a8604c0c9f20
Jeff Perkinson <jmperkinson@gmail.com>
2020-03-22 21:29:19 -0400

    Remove unneeded comments.

--- inc/quiz.php
+++ inc/quiz.php
@@ -56,49 +56,7 @@ if(isset($_SESSION["userAnswer"][($questionNumber - 1)])) {
     }
 }
 
-// var_dump($_SESSION);
-
 // If question number exceeds the set number of questions, redirect to game over page
 if($questionNumber > $numberOfQuestions) {
     header("location:game-over.php");
 }
-
-/*
-    If the server request was of type POST
-        Check if the user's answer was equal to the correct answer.
-        If it was correct:
-            1. Assign a congratulutory string to the toast variable
-            2. Increment the session variable that holds the total number correct by one.
-        Otherwise:
-            1. Assign a bummer message to the toast variable.
-*/
-
-/*
-    Check if a session variable has ever been set/created to hold the indexes of questions already asked.
-    If it has NOT: 
-        1. Create a session variable to hold used indexes and initialize it to an empty array.
-        2. Set the show score variable to false.
-*/
-
-
-/*
-  If the number of used indexes in our session variable is equal to the total number of questions
-  to be asked:
-        1.  Reset the session variable for used indexes to an empty array 
-        2.  Set the show score variable to true.
-
-  Else:
-    1. Set the show score variable to false 
-    2. If it's the first question of the round:
-        a. Set a session variable that holds the total correct to 0. 
-        b. Set the toast variable to an empty string.
-        c. Assign a random number to a variable to hold an index. Continue doing this
-            for as long as the number generated is found in the session variable that holds used indexes.
-        d. Add the random number generated to the used indexes session variable.      
-        e. Set the individual question variable to be a question from the questions array and use the index
-            stored in the variable in step c as the index.
-        f. Create a variable to hold the number of items in the session variable that holds used indexes
-        g. Create a new variable that holds an array. The array should contain the correctAnswer,
-            firstIncorrectAnswer, and secondIncorrect answer from the variable in step e.
-        h. Shuffle the array from step g.
-*/
