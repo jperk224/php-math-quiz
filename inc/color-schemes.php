c892274943014bfff66ec848c74a04861ced3f33
Jeff Perkinson <jmperkinson@gmail.com>
2020-03-24 12:29:50 -0400

    Add improved contrast to color-schemes.

--- inc/color-schemes.php
+++ inc/color-schemes.php
@@ -11,35 +11,35 @@ function cssApplicator($selector, $property, $value) {
 
 // schemes taken from triadic color theory
 // (https://www.canva.com/colors/color-wheel/)
-// 1. Main: #1ECBE1 Button: #E11ECB Font: #CBE11E
-// 2. Main: #32CDBD Button: #BD32CD Font: #BD32CD
-// 3. Main: #43bc91 Button: #BC9143 Font: #9143BC
+// Shades adjusted to keep contrast raio greater than 3.0
+// 2. Main: #32CDBD Button: #BD32CD Font: #FFF
+// 3. Main: #43bc91 Button: #BC9143 Font: #FFF
 
 
 // Display one of 3 schemes based on randomization
 function chooseRandomScheme() {
     $colorSchemeNumber = rand(1,3);   // randomly choose one of three schemes
     switch($colorSchemeNumber) {
-        case 1: // Good
+        case 1:
             cssApplicator("body", "background-color", "#1ECBE1");
-            cssApplicator("body", "color", "#E11ECB");
-            cssApplicator(".btn", "background-color", "#CBE11E");
-            cssApplicator(".btn", "color", "#E11ECB");
+            cssApplicator("body", "color", "#595959");
+            cssApplicator(".btn", "background-color", "#AF189D");
+            cssApplicator(".btn", "color", "#FFF");
             cssApplicator(".btn", "border", "none");      // remove the button border
             break;
-        case 2: // Good
+        case 2: 
             cssApplicator("body", "background-color", "#32CDBD");
-            cssApplicator("body", "color", "#BD32CD");
+            cssApplicator("body", "color", "#90269C");
             cssApplicator(".btn", "background-color", "#CDBD32");
-            cssApplicator(".btn", "color", "#BD32CD");
+            cssApplicator(".btn", "color", "#9F2AAC");
             cssApplicator(".btn", "border", "none");
             break;
-        case 3: // Good
+        case 3:
         default:    // default should not be reached, but included as fallback
             cssApplicator("body", "background-color", "#43bc91");
-            cssApplicator("body", "color", "#9143BC");
+            cssApplicator("body", "color", "#743696");
             cssApplicator(".btn", "background-color", "#BC9143");
-            cssApplicator(".btn", "color", "#9143BC");
+            cssApplicator(".btn", "color", "#612D7C");
             cssApplicator(".btn", "border", "none");
             break;
     }
